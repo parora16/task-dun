5fbf0ae46c6948476828c4ed3e62707976c09aae
PrashantArora <prashant.arora@telus.com>
2022-01-26 18:27:35 +0530

    fix: variable fix

--- main/main.tf
+++ main/main.tf
@@ -5,7 +5,6 @@ module "vpc" {
   routing_mode                           = var.routing_mode
   project_id                             = var.project_id
   description                            = var.description
-  shared_vpc_host                        = var.shared_vpc_host
   delete_default_internet_gateway_routes = var.delete_default_internet_gateway_routes
   mtu                                    = var.mtu
 }

cafcc6003cf298955b1b4528c08dc94c67274a7a
PrashantArora <prashant.arora@telus.com>
2022-01-26 20:08:54 +0530

    fix: name fix

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,4 +1,4 @@
-resource "google_compute_network" "network" {
+resource "google_compute_network" "vpc" {
   name                            = var.network_name
   auto_create_subnetworks         = var.auto_create_subnetworks
   routing_mode                    = var.routing_mode

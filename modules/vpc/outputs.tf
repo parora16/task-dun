dfd35db4e767a58b4d004533b9086603586531aa
PrashantArora <prashant.arora@telus.com>
2022-01-26 18:28:45 +0530

    fix: variable fixed

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -17,8 +17,3 @@ output "network_self_link" {
   value       = google_compute_network.network.self_link
   description = "The URI of the VPC being created"
 }
-
-output "project_id" {
-  value       = var.shared_vpc_host && length(google_compute_shared_vpc_host_project.shared_vpc_host) > 0 ? google_compute_shared_vpc_host_project.shared_vpc_host.*.project[0] : google_compute_network.network.project
-  description = "VPC project id"
-}

adcbb9408ba6e9e3d4e437f11048425efefa7d28
PrashantArora <prashant.arora@telus.com>
2022-01-26 20:09:57 +0530

    fix: change reverted

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,4 +1,4 @@
-resource "google_compute_network" "vpc" {
+resource "google_compute_network" "network" {
   name                            = var.network_name
   auto_create_subnetworks         = var.auto_create_subnetworks
   routing_mode                    = var.routing_mode
